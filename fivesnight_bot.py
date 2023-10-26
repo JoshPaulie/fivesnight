d97447bb26b6ed5dbdee4e0d1289a10a4e0c4a52
JoshPaulie (Bexli) <30572280+JoshPaulie@users.noreply.github.com>
2023-10-25 19:39:47 -0500

    Organize objects into relevant namespaces

--- fivesnight_bot.py
+++ fivesnight_bot.py
@@ -39,47 +39,166 @@ class AssignedPlayer:
     role: str
 
 
-# Helpers
-def assign_roles(team: list[DiscordUser]) -> list[AssignedPlayer]:
-    """'Assigns' a team roles by shuffling their order, then grabbing the next role (from top to bottom).
-    Any leftover team members are given the 'Fill' role.
-    This allows for more than 10 people to queue up, and let the bot randomly pick who gets to play
-    """
-    random.shuffle(team)
-    roles = "Support Bottom Middle Jungle Top".split()
-    assigned_roles = []
-    for player in team:
-        if roles:
-            selected_role = roles.pop()
-            assigned_roles.append(AssignedPlayer(player, selected_role))
-            continue
-        # If no more roles, give the extras "fill"
-        assigned_roles.append(AssignedPlayer(player, "Fill"))
-    return assigned_roles
+class Helpers:
+    """Various helpers"""
+
+    @staticmethod
+    def assign_roles(team: list[DiscordUser]) -> list[AssignedPlayer]:
+        """'Assigns' a team roles by shuffling their order, then grabbing the next role (from top to bottom).
+        Any leftover team members are given the 'Fill' role.
+        This allows for more than 10 people to queue up, and let the bot randomly pick who gets to play
+        """
+        random.shuffle(team)
+        roles = "Support Bottom Middle Jungle Top".split()
+        assigned_roles = []
+        for player in team:
+            if roles:
+                selected_role = roles.pop()
+                assigned_roles.append(AssignedPlayer(player, selected_role))
+                continue
+            # If no more roles, give the extras "fill"
+            assigned_roles.append(AssignedPlayer(player, "Fill"))
+        return assigned_roles
+
+    @staticmethod
+    def create_bullet_points(lst: list[Any]):
+        """Takes list of items, return bullet point versions"""
+        return [f"- {item}\n" for item in lst]
+
+    @staticmethod
+    def list_to_multiline_string(lst: list[str]) -> str:
+        """Helper for writing multiline embed descriptions, field values, etc, easier to hardcode"""
+        result = ""
+        for line in lst:
+            result += f"{line}\n"
+        return result
+
+    @staticmethod
+    def calc_winrate(wins: int, games: int) -> str:
+        """Returns user winrate in percentage form (with symbol)
+
+        Example
+            calc_winrate(2, 4) -> 50.0%
+        """
+        ratio = wins / games
+        ratio_percentage = ratio * 100
+        return f"{round(ratio_percentage, 1)}%"
 
 
-def create_bullet_points(lst: list[Any]):
-    """Takes list of items, return bullet point versions"""
-    return [f"- {item}\n" for item in lst]
+class Views:
+    class DeleteThisMessageView(discord.ui.View):
+        def __init__(self, *, timeout: float | None = 180):
+            super().__init__(timeout=timeout)
 
+        @discord.ui.button(label="Delete", style=discord.ButtonStyle.danger)
+        async def delete_me(self, interaction: discord.Interaction, button: discord.ui.Button):
+            assert interaction.message
+            await interaction.message.delete()
 
-def list_to_multiline_string(lst: list[str]) -> str:
-    """Helper for writing multiline embed descriptions, field values, etc, easier to hardcode"""
-    result = ""
-    for line in lst:
-        result += f"{line}\n"
-    return result
+    class TeamCreationView(discord.ui.View):
+        def __init__(self, *, timeout: float | None = 180, organizer: DiscordUser):
+            super().__init__(timeout=timeout)
+            self.timeout_amount = timeout
+            self.organizer = organizer
+            self.queue: list[DiscordUser] = []
+            self.team_one: list[DiscordUser] = []
+            self.team_two: list[DiscordUser] = []
 
+        # Join queue
+        @discord.ui.button(label="Join", style=discord.ButtonStyle.green)
+        async def join(self, interaction: discord.Interaction, button: discord.ui.Button):
+            user = interaction.user
+            # Check: User is already in queue, nothing to do
+            if user in self.queue:
+                await interaction.response.defer()
+                return
+            self.queue.append(user)
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    title=f"You've joined the queue!",
+                    description=f"Current queue size: ({len(self.queue)})",
+                    color=discord.Color.green(),
+                ),
+                ephemeral=True,
+            )
 
-def calc_winrate(wins: int, games: int) -> str:
-    """Returns user winrate in percentage form (with symbol)
+        # Leave queue
+        @discord.ui.button(label="Leave", style=discord.ButtonStyle.red)
+        async def leave(self, interaction: discord.Interaction, button: discord.ui.Button):
+            user = interaction.user
+            # Check: User isn't in queue, nothing to do
+            if user not in self.queue:
+                await interaction.response.defer()
+                return
+            self.queue.remove(user)
+            await interaction.response.send_message(
+                embed=discord.Embed(title="You've left the queue.", color=discord.Color.red()),
+                ephemeral=True,
+            )
 
-    Example
-        calc_winrate(2, 4) -> 50.0%
-    """
-    ratio = wins / games
-    ratio_percentage = ratio * 100
-    return f"{round(ratio_percentage, 1)}%"
+        # Create teams
+        @discord.ui.button(label="Create Teams", style=discord.ButtonStyle.blurple)
+        async def create(self, interaction: discord.Interaction, button: discord.ui.Button):
+            # Check: Only organizer can start
+            if not interaction.user == self.organizer:
+                await interaction.response.send_message(
+                    f"Only the organizer ({self.organizer.name}) can finish the queue", ephemeral=True
+                )
+                return
+            # Shuffle the queue order 3 times
+            for _ in range(3):
+                random.shuffle(self.queue)
+            # Split queue in half, cram into teams
+            self.team_one = self.queue[len(self.queue) // 2 :]
+            self.team_two = self.queue[: len(self.queue) // 2]
+            # Put the two teams into the bots memory for recording match outcomes later
+            bot.team_one = self.team_one
+            bot.team_two = self.team_two
+            # End the view
+            self.stop()
+
+        @discord.ui.button(label="Check Queue", style=discord.ButtonStyle.gray, row=1)
+        async def show_queue(self, interaction: discord.Interaction, button: discord.ui.Button):
+            queue_len = len(self.queue)
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    title=f"There {'are' if queue_len != 1 else 'is'} ({queue_len}) {'people' if queue_len != 1 else 'person'} in the queue",
+                    description="\n".join(Helpers.create_bullet_points(self.queue)),
+                    color=discord.Color.greyple(),
+                ),
+                ephemeral=True,
+            )
+
+    class RecordLastMatchView(discord.ui.View):
+        def __init__(self, *, timeout: float | None = 180):
+            super().__init__(timeout=timeout)
+            self.winning_team = []
+
+        @staticmethod
+        def record_team_one(winning_team: bool):
+            """Update JSON file"""
+            match_manager.add_player_match([player.id for player in bot.team_one], won_game=winning_team)
+
+        @staticmethod
+        def record_team_two(winning_team: bool):
+            """Update JSON file"""
+            match_manager.add_player_match([player.id for player in bot.team_two], won_game=winning_team)
+
+        # Team One won
+        @discord.ui.button(label="Team One", style=discord.ButtonStyle.blurple)
+        async def team_one(self, interaction: discord.Interaction, button: discord.ui.Button):
+            self.record_team_one(winning_team=True)
+            self.record_team_two(winning_team=False)
+            self.winning_team = bot.team_one
+            self.stop()
+
+        # Team One won
+        @discord.ui.button(label="Team Two", style=discord.ButtonStyle.red)
+        async def team_two(self, interaction: discord.Interaction, button: discord.ui.Button):
+            self.record_team_one(winning_team=False)
+            self.record_team_two(winning_team=True)
+            self.winning_team = bot.team_two
+            self.stop()
 
 
 # The bot class itself
@@ -114,7 +233,7 @@ bot = FivesnightBot(intents=discord.Intents.all())
 
 # Sync command
 @bot.command(name="sync", description="[Meta] Syncs commands to server")
-async def sync(ctx: commands.Context):
+async def sync_commands(ctx: commands.Context):
     if not await bot.is_owner(ctx.author):
         await ctx.reply("Only jarsh needs to use this 😬", ephemeral=True)
         return
@@ -123,100 +242,14 @@ async def sync(ctx: commands.Context):
     await ctx.reply(f"Bot commands synced to {ctx.guild.name}")
 
 
-# Clean up view for select messages
-class DeleteThisMessageView(discord.ui.View):
-    def __init__(self, *, timeout: float | None = 180):
-        super().__init__(timeout=timeout)
-
-    @discord.ui.button(label="Delete this message", style=discord.ButtonStyle.danger)
-    async def delete_me(self, interaction: discord.Interaction, button: discord.ui.Button):
-        assert interaction.message
-        await interaction.message.delete()
-
-
-# Team creation
-class TeamCreationView(discord.ui.View):
-    def __init__(self, *, timeout: float | None = 180, organizer: DiscordUser):
-        super().__init__(timeout=timeout)
-        self.timeout_amount = timeout
-        self.organizer = organizer
-        self.queue: list[DiscordUser] = []
-        self.team_one: list[DiscordUser] = []
-        self.team_two: list[DiscordUser] = []
-
-    # Join queue
-    @discord.ui.button(label="Join", style=discord.ButtonStyle.green)
-    async def join(self, interaction: discord.Interaction, button: discord.ui.Button):
-        user = interaction.user
-        # Check: User is already in queue, nothing to do
-        if user in self.queue:
-            await interaction.response.defer()
-            return
-        self.queue.append(user)
-        await interaction.response.send_message(
-            embed=discord.Embed(
-                title=f"You've joined the queue!",
-                description=f"Current queue size: ({len(self.queue)})",
-                color=discord.Color.green(),
-            ),
-            ephemeral=True,
-        )
-
-    # Leave queue
-    @discord.ui.button(label="Leave", style=discord.ButtonStyle.red)
-    async def leave(self, interaction: discord.Interaction, button: discord.ui.Button):
-        user = interaction.user
-        # Check: User isn't in queue, nothing to do
-        if user not in self.queue:
-            await interaction.response.defer()
-            return
-        self.queue.remove(user)
-        await interaction.response.send_message(
-            embed=discord.Embed(title="You've left the queue.", color=discord.Color.red()),
-            ephemeral=True,
-        )
-
-    # Create teams
-    @discord.ui.button(label="Create Teams", style=discord.ButtonStyle.blurple)
-    async def create(self, interaction: discord.Interaction, button: discord.ui.Button):
-        # Check: Only organizer can start
-        if not interaction.user == self.organizer:
-            await interaction.response.send_message(
-                f"Only the organizer ({self.organizer.name}) can finish the queue", ephemeral=True
-            )
-            return
-        # Shuffle the queue order 3 times
-        for _ in range(3):
-            random.shuffle(self.queue)
-        # Split queue in half, cram into teams
-        self.team_one = self.queue[len(self.queue) // 2 :]
-        self.team_two = self.queue[: len(self.queue) // 2]
-        # Put the two teams into the bots memory for recording match outcomes later
-        bot.team_one = self.team_one
-        bot.team_two = self.team_two
-        # End the view
-        self.stop()
-
-    @discord.ui.button(label="Check Queue", style=discord.ButtonStyle.gray, row=1)
-    async def show_queue(self, interaction: discord.Interaction, button: discord.ui.Button):
-        queue_len = len(self.queue)
-        await interaction.response.send_message(
-            embed=discord.Embed(
-                title=f"There {'are' if queue_len != 1 else 'is'} ({queue_len}) {'people' if queue_len != 1 else 'person'} in the queue",
-                description="\n".join(create_bullet_points(self.queue)),
-                color=discord.Color.greyple(),
-            ),
-            ephemeral=True,
-        )
-
-
+# Create teams command
 @bot.tree.command(
     name="create",
     description="Create a virtual queue for players to join (creates teams & assigns roles)",
 )
-async def create(interaction: discord.Interaction):
+async def create_teams(interaction: discord.Interaction):
     # create team creation object (department of redundancy department)
-    team_creator = TeamCreationView(organizer=interaction.user)
+    team_creator = Views.TeamCreationView(organizer=interaction.user)
     # Send it with a cutie embed
     await interaction.response.send_message(
         embed=discord.Embed(
@@ -229,8 +262,8 @@ async def create(interaction: discord.Interaction):
     # Wait for view to finish (it's stopped by the organizer)
     await team_creator.wait()
     # Grab the two teams from the creator
-    team_one = assign_roles(team_creator.team_one)
-    team_two = assign_roles(team_creator.team_two)
+    team_one = Helpers.assign_roles(team_creator.team_one)
+    team_two = Helpers.assign_roles(team_creator.team_two)
     # Bypass team minimum check if in debug debug channel
     if interaction.channel != DEBUG_CHANNEL:
         # check: if either team is empty, stop the show (and shame them)
@@ -249,7 +282,7 @@ async def create(interaction: discord.Interaction):
                     description="One or more teams had 0 members.",
                     color=discord.Color.greyple(),
                 ),
-                view=DeleteThisMessageView(),
+                view=Views.DeleteThisMessageView(),
             )
             return
     # "Clean up" old message so people can't click the buttons
@@ -271,45 +304,13 @@ async def create(interaction: discord.Interaction):
             description="After the game, use the `/record` command to log the outcome of the match!",
             color=discord.Color.blurple(),
         ).set_footer(text="Your 5v5 winrate can be checked with /winrate"),
-        view=DeleteThisMessageView(),
+        view=Views.DeleteThisMessageView(),
     )
 
 
 # Record last game played
-class RecordLastMatchView(discord.ui.View):
-    def __init__(self, *, timeout: float | None = 180):
-        super().__init__(timeout=timeout)
-        self.winning_team = []
-
-    @staticmethod
-    def record_team_one(winning_team: bool):
-        """Update JSON file"""
-        match_manager.add_player_match([player.id for player in bot.team_one], won_game=winning_team)
-
-    @staticmethod
-    def record_team_two(winning_team: bool):
-        """Update JSON file"""
-        match_manager.add_player_match([player.id for player in bot.team_two], won_game=winning_team)
-
-    # Team One won
-    @discord.ui.button(label="Team One", style=discord.ButtonStyle.blurple)
-    async def team_one(self, interaction: discord.Interaction, button: discord.ui.Button):
-        self.record_team_one(winning_team=True)
-        self.record_team_two(winning_team=False)
-        self.winning_team = bot.team_one
-        self.stop()
-
-    # Team One won
-    @discord.ui.button(label="Team Two", style=discord.ButtonStyle.red)
-    async def team_two(self, interaction: discord.Interaction, button: discord.ui.Button):
-        self.record_team_one(winning_team=False)
-        self.record_team_two(winning_team=True)
-        self.winning_team = bot.team_two
-        self.stop()
-
-
 @bot.tree.command(name="record", description="Record the last match & indicate who won")
-async def record(interaction: discord.Interaction):
+async def record_last_outcome(interaction: discord.Interaction):
     # check for recent match played
     if all([not bot.team_one, not bot.team_two]):
         await interaction.response.send_message(
@@ -317,10 +318,10 @@ async def record(interaction: discord.Interaction):
                 title="There hasn't been a match played recently.",
                 color=discord.Color.greyple(),
             ).set_footer(text="If there was a mistake recording a recent match, message jarsh"),
-            view=DeleteThisMessageView(),
+            view=Views.DeleteThisMessageView(),
         )
         return
-    record_match_view = RecordLastMatchView()
+    record_match_view = Views.RecordLastMatchView()
     await interaction.response.send_message(
         embed=discord.Embed(title="Who won the last game?", color=discord.Color.greyple()),
         view=record_match_view,
@@ -344,7 +345,7 @@ async def record(interaction: discord.Interaction):
 
 # Winrate "leaderboard"
 @bot.tree.command(name="winrates", description="Get everyone's win rates!")
-async def winrates(interaction: discord.Interaction):
+async def check_group_winrates(interaction: discord.Interaction):
     # Create embed
     winrates_embed = discord.Embed(title="Win rates!", color=discord.Color.blurple())
     for record in match_manager.get_match_history().items():
@@ -354,7 +355,7 @@ async def winrates(interaction: discord.Interaction):
         assert interaction.guild
         winrates_embed.add_field(
             name=interaction.guild.get_member(user_id),
-            value=f"{games_won}/{games_played} ({calc_winrate(games_won, games_played)})",
+            value=f"{games_won}/{games_played} ({Helpers.calc_winrate(games_won, games_played)})",
         )
     # Check: no winrates to display
     if not len(winrates_embed.fields):
@@ -363,11 +364,11 @@ async def winrates(interaction: discord.Interaction):
                 title="No games have been played yet. 🥲",
                 color=discord.Color.greyple(),
             ),
-            view=DeleteThisMessageView(),
+            view=Views.DeleteThisMessageView(),
         )
         return
     # Send winrates embed
-    await interaction.response.send_message(embed=winrates_embed, view=DeleteThisMessageView())
+    await interaction.response.send_message(embed=winrates_embed, view=Views.DeleteThisMessageView())
 
 
 def main():
